d13893f5a772d26d90f9c9db022927dddb4d7e1e
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-18 19:13:23 -0400

    display tours done

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div class="title">
+        <h2>our tours</h2>
+        <div class="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour}></Tour>;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
