53f5988091be414dc9e862befc4e0dd546dc4cc7
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-23 22:43:38 -0400

    autoplay setup and alternative way

--- 07-slider/setup/src/Alternative.js
+++ 07-slider/setup/src/Alternative.js
@@ -5,20 +5,46 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, people]);
+
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
+
+  // useEffect(() => {
+  //   const lastIndex = people.length - 1;
+  //   if (index < 0) {
+  //     setIndex(lastIndex);
+  //   }
+  //   if (index > lastIndex) {
+  //     setIndex(0);
+  //   }
+  // }, [index, people]);
 
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(index + 1);
-    }, 3000);
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+    }, 5000);
     return () => clearInterval(slider);
   }, [index]);
 
@@ -55,10 +81,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => setIndex(index + 1)}>
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
