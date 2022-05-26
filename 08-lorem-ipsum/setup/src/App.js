071f7b3ef7edc0e05f53012ef762a84d17ab52d8
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-25 23:51:46 -0400

    lorem ipsum done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
 
   return (
@@ -26,12 +33,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, in?
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, in?
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
