c6f31e0a2c43ca9e23aeebfcf18c1abf3407c64d
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-27 23:17:55 -0400

    edit item

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -19,6 +19,19 @@ function App() {
       showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
       // deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value');
     } else {
       // show alert
       showAlert(true, 'success', 'item added to the list');
@@ -38,6 +51,12 @@ function App() {
   const removeItem = (id) => {
     showAlert(true, 'danger', 'item removed');
     setList(list.filter((item) => item.id !== id));
+  };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
   };
   return (
     <section className="section-center">
@@ -59,7 +78,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className="clear-btn" onClick={clearList}>
             clear items
           </button>
