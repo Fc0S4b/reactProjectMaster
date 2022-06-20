0dfd80b979d29d5156aca7fb07ca5b397cb24484
Fc0S4b <fcoj.sabugo@gmail.com>
2022-06-19 22:57:37 -0400

    about and error

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        quibusdam facilis laborum corrupti ab laboriosam, laudantium aperiam
+        perspiciatis fugit explicabo omnis maxime. Obcaecati, quos. Velit
+        laborum aspernatur vel, saepe magni ipsa atque tenetur hic ipsam eos est
+        earum tempora. Aspernatur.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
